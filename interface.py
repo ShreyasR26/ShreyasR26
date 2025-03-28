903723ef38dfb3d01c78cb65dad4e6e9684e6124
Shreyas Ramani <43936265+ShreyasR26@users.noreply.github.com>
2025-03-28 15:37:32 -0700

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,18 @@
+from neo4j import GraphDatabase
+
+class Interface:
+    def __init__(self, uri, user, password):
+        self._driver = GraphDatabase.driver(uri, auth=(user, password), encrypted=False)
+        self._driver.verify_connectivity()
+
+    def close(self):
+        self._driver.close()
+
+    def bfs(self, start_node, last_node):
+        # TODO: Implement this method
+        raise NotImplementedError
+
+    def pagerank(self, max_iterations, weight_property):
+        # TODO: Implement this method
+        raise NotImplementedError
+
